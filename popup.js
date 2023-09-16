fb498a09904bda83ba79ac30dde91f1548619ec2
ryanufl <rpallack@ufl.edu>
2023-09-16 19:30:54 -0400

    get rid of old

--- popup.js
+++ popup.js
@@ -43,29 +43,6 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
-    const bitcoinPrice = document.getElementById('bitcoinPrice');
-    const tooltip = document.getElementById('tooltip');
-    var closeButton = document.getElementById('closeButton');
-    var reportButton = document.getElementById('reportButton');
-    var logoButton = document.getElementById('logoButton');
-
-    logoButton.addEventListener('click', function () {
-        var bitstopURL = 'https://bitstop.co/';
-        chrome.tabs.create({ url: bitstopURL });
-        console.log("LOGO")
-    });
-
-    reportButton.addEventListener('click', function () {
-        var reportFormURL = 'https://forms.gle/fZyhk7fVLRZAZB8A6';
-        chrome.tabs.create({ url: reportFormURL });
-        console.log("REPORT")
-    });
-
-    closeButton.addEventListener('click', function () {
-        window.close();
-        //chrome.runtime.sendMessage({ action: 'closeExtension' });
-        console.log("CLOSE")
-    });
     document.getElementById("dollarPrices").addEventListener("click", function () {
         // document.getElementById("bitcoinPrices").disabled = false
         // document.getElementById("dollarPrices").disabled = true
