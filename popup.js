b5c0df06e7f8e0c1f65eefc8cd3e703237b84225
ryanufl <rpallack@ufl.edu>
2023-09-16 17:30:56 -0400

    Added live bitcoin price:

--- popup.js
+++ popup.js
@@ -1,8 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+    async function getCurrentBitcoinPrice() {
+        // Define the API endpoint URL
+        const apiUrl = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&precision=5';
+        let bitcoin = -1
+        try {
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+            const price = data.bitcoin.usd;
+            console.log(price);
+            if (price !== undefined) {
+                bitcoin = price;
+            } else {
+                bitcoin = -1;
+            }
+        } catch (error) {
+            bitcoin = -2;
+        }
+        return bitcoin.toFixed(2);
+
+    }
+    (async () => {
+        document.getElementById('livePrice').innerText = "$"+(await getCurrentBitcoinPrice())
+    })()
+
+
     document.getElementById("bitcoinPrices").addEventListener("click", function () {
-        document.getElementById("bitcoinPrices").disabled = true
-        document.getElementById("dollarPrices").disabled = false
-        document.getElementById("bothPrices").disabled = false
+        console.log("ADDED EVENT LISTENER")
+        // document.getElementById("bitcoinPrices").disabled = true
+        // document.getElementById("dollarPrices").disabled = false
+        // document.getElementById("bothPrices").disabled = false
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, { action: "justBTC" }, function (response) {
                 if (chrome.runtime.lastError) {
@@ -15,9 +42,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     document.getElementById("dollarPrices").addEventListener("click", function () {
-        document.getElementById("bitcoinPrices").disabled = false
-        document.getElementById("dollarPrices").disabled = true
-        document.getElementById("bothPrices").disabled = false
+        // document.getElementById("bitcoinPrices").disabled = false
+        // document.getElementById("dollarPrices").disabled = true
+        // document.getElementById("bothPrices").disabled = false
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, { action: "justMoney" }, function (response) {
                 if (chrome.runtime.lastError) {
@@ -29,9 +56,11 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
     document.getElementById("bothPrices").addEventListener("click", function () {
-        document.getElementById("bitcoinPrices").disabled = false
-        document.getElementById("dollarPrices").disabled = false
-        document.getElementById("bothPrices").disabled = true
+        document.getElementById("bothPrices").setAttribute("background-color", "yellow")
+        document.getElementById("bothPrices").setAttribute("position", "relative")
+        // document.getElementById("bitcoinPrices").disabled = false
+        // document.getElementById("dollarPrices").disabled = false
+        // document.getElementById("bothPrices").disabled = true
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, { action: "both" }, function (response) {
                 if (chrome.runtime.lastError) {
@@ -42,30 +71,5 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         });
     });
-    
-});
 
-
-document.addEventListener("DOMContentLoaded", function () {
-    const bitcoinPrice = document.getElementById('bitcoinPrice');
-    const tooltip = document.getElementById('tooltip');
-
-    bitcoinPrice.addEventListener('mouseover', () => {
-        // Customize tooltip content and position
-        tooltip.textContent = "Bitcoin is a decentralized digital currency...";
-        tooltip.style.display = 'block';
-        tooltip.style.left = bitcoinPrice.offsetLeft + 'px'; // Adjust position as needed
-        tooltip.style.top = (bitcoinPrice.offsetTop + bitcoinPrice.offsetHeight) + 'px'; // Adjust position as needed
-    });
-
-    bitcoinPrice.addEventListener('mouseout', () => {
-        // Hide the tooltip
-        tooltip.style.display = 'none';
-    });
-});
-
-const button = document.getElementById('bitcoin');
-
-button.addEventListener('click', function () {
-    button.classList.toggle('inverted');
 });
