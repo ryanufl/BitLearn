72e8754573b9619df664801d9f8508161b307c74
Vishwa Krishnan <vishwakrishnan02@gmail.com>
2023-09-16 14:31:21 -0400

    Image updates and report, close, & other features

--- popup.js
+++ popup.js
@@ -1,18 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
     const bitcoinPrice = document.getElementById('bitcoinPrice');
     const tooltip = document.getElementById('tooltip');
+    var closeButton = document.getElementById('closeButton');
+    var reportButton = document.getElementById('reportButton');
+    var logoButton = document.getElementById('logoButton');
 
-    bitcoinPrice.addEventListener('mouseover', () => {
-        // Customize tooltip content and position
-        tooltip.textContent = "Bitcoin is a decentralized digital currency...";
-        tooltip.style.display = 'block';
-        tooltip.style.left = bitcoinPrice.offsetLeft + 'px'; // Adjust position as needed
-        tooltip.style.top = (bitcoinPrice.offsetTop + bitcoinPrice.offsetHeight) + 'px'; // Adjust position as needed
-    });
+    // bitcoinPrice.addEventListener('mouseover', () => {
+    //     // Customize tooltip content and position
+    //     tooltip.textContent = "Bitcoin is a decentralized digital currency...";
+    //     tooltip.style.display = 'block';
+    //     tooltip.style.left = bitcoinPrice.offsetLeft + 'px'; // Adjust position as needed
+    //     tooltip.style.top = (bitcoinPrice.offsetTop + bitcoinPrice.offsetHeight) + 'px'; // Adjust position as needed
+    // });
 
-    bitcoinPrice.addEventListener('mouseout', () => {
-        // Hide the tooltip
-        tooltip.style.display = 'none';
+    // bitcoinPrice.addEventListener('mouseout', () => {
+    //     // Hide the tooltip
+    //     tooltip.style.display = 'none';
+    // });
+
+    logoButton.addEventListener('click', function() {
+        var bitstopURL = 'https://bitstop.co/';
+        chrome.tabs.create({url: bitstopURL});
+        console.log("LOGO")
+     });
+    
+    reportButton.addEventListener('click', function() {
+        var reportFormURL = 'https://forms.gle/fZyhk7fVLRZAZB8A6';
+        chrome.tabs.create({url: reportFormURL});
+        console.log("REPORT")
+     });
+
+    closeButton.addEventListener('click', function() { window.close(); 
+      //chrome.runtime.sendMessage({ action: 'closeExtension' });
+      console.log("CLOSE")
     });
 });
 
