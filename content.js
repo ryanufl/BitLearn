71afcc6e68cbbf55ae2c08e9e1688e770a9de855
ryanufl <rpallack@ufl.edu>
2023-09-16 02:40:04 -0400

    Added better code for replacing

--- content.js
+++ content.js
@@ -1,2 +1,13 @@
-let replaced = document.body.innerHTML.replaceAll("replaceAll()", 'FUNCTION');
-document.body.innerHTML = replaced;
+// Define the regular expression for dollar values
+var dollarValueRegex = /\$\d+(\.\d{2})?/g;
+
+// Get the HTML content of the page
+var pageContent = document.body.innerHTML;
+
+// Replace every dollar value with itself plus " this is too much"
+var newContent = pageContent.replace(dollarValueRegex, function(match) {
+  return match + "ENTER BIT COIN PRICE HERE";
+});
+
+// Set the HTML content of the page
+document.body.innerHTML = newContent;
